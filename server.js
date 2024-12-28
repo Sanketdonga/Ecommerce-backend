7471f9ace8d028d1ea16b8f8170c14e11c630653
sanketdonga <sanketdongasanket@gmail.com>
2024-12-28 16:02:44 +0530

    update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require('path');
 const authRouter = require("./routes/auth/auth-routes");
 const adminProductsRouter = require("./routes/admin/products-routes");
 const adminOrderRouter = require("./routes/admin/order-routes");
